c173051849d324b5048ccd8b5a29837452e4537b
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2024-11-25 18:47:21 +0300

    loglama mesajı

--- backend/LinkShortener/LinkShortener.Api/Middlewares/ErrorMiddleware.cs
+++ backend/LinkShortener/LinkShortener.Api/Middlewares/ErrorMiddleware.cs
@@ -23,30 +23,26 @@ namespace LinkShortener.Api.Middlewares
             }
             catch (CustomException customEx)
             {
-                // CustomException'dan log bilgisi
+                // CustomExceptiondan log bilgisi
                 await LogAndHandleException(context, logService, customEx.Status, customEx.Message, customEx, "Warning");
             }
             catch (Exception ex)
             {
                 // Genel Exception
-                await LogAndHandleException(context, logService, "Critical", "Unhandled exception occurred", ex, "Critical");
+                await LogAndHandleException(context, logService, "Critical", "bilinmeyen hata oluştu", ex, "Critical");
             }
         }
 
         private async Task LogAndHandleException(HttpContext context, ILogService logService, string level, string message, Exception ex, string logLevel)
         {
-            // Controller ve Action bilgisi
             var controller = context.GetRouteValue("controller")?.ToString();
             var action = context.GetRouteValue("action")?.ToString();
             var path = context.Request.Path;
 
-            // Log mesajı oluştur
             var logMessage = $"Exception in {controller}/{action} (Path: {path}): {message}";
 
-            // Logla
             logService.LogError(level, logMessage, ex);
 
-            // HTTP response hazırlama
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             var response = new
             {
