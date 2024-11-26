c58901e96de810164d07f882f2b94354db561b2d
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2024-11-26 16:36:46 +0300

    sol kısım tasarımları yapıldı

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ function App() {
   return (
 <>
 
-<div className="h-screen flex items-center justify-center">
-  <div className="w-[85%] h-[90%] bg-white/20 z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl flex justify-start items-start p-4">
-    {/* İçerik */}
+      <div className="h-screen flex items-center justify-center">
+        <div className="w-[85%] h-[90%] bg-white/20 z-10 backdrop-filter backdrop-blur-lg shadow-lg rounded-2xl flex justify-start items-start p-4">
+          {/* İçerik */}
           <div className="flex flex-row gap-5 w-full h-full p-2">
             {/* Sol Sütun */}
             <div className="flex flex-col gap-5 w-[20%] h-full">
               {/* Birinci Kutu */}
-              <div className="w-full h-[70%] bg-white rounded-lg shadow-md relative">
+              <div className="w-full h-[75%] bg-white rounded-lg shadow-md relative">
                 {/* Mavi alan */}
                 <div className="w-full h-full bg-blue-500"
                   style={{
@@ -37,21 +37,56 @@ function App() {
 
 
 
-        {/* İkinci Kutu */}
-        <div className="w-full h-[30%] bg-white rounded-lg shadow-md"></div>
-      </div>
+              {/* İkinci Kutu */}
+              <div className="w-full h-[25%] bg-blue-500 rounded-lg shadow-md flex justify-center items-center relative">
 
-      {/* Sağ Sütun */}
-      <div className="flex flex-col gap-4 w-[80%] h-full">
-        {/* Üçüncü Kutu */}
-        <div className="w-full h-[20%] bg-gray-300 rounded-lg shadow-md"></div>
-        <div className="w-full h-[60%] bg-gray-300 rounded-lg shadow-md"></div>
-        {/* Dördüncü Kutu */}
-        <div className="w-full h-[20%] bg-gray-300 rounded-lg shadow-md"></div>
+
+                <img className="w-[70%] h-[90%]" src="/4.png" />
+                {/* Yazı Overlay */}
+
+                {/* derece */}
+                <div className="absolute top-2 left-2 w-[15%] h-[20%] flex justify-center items-center bg-white/50 backdrop-blur-sm rounded-lg">
+                  <p className="text-white text-sm font-medium">
+                    19°C
+                  </p>
+                </div>
+
+                {/* gun bilgisi */}
+                <div className="absolute top-2 right-2 w-[40%] h-[20%] flex justify-center items-center bg-white/50 backdrop-blur-sm rounded-lg">
+                  <p className="text-white text-sm font-medium">
+                    Salı  2024
+                  </p>
+                </div>
+                {/* düşük ve alcak sıcaklık  */}
+
+                {/* derece */}
+                <div className="absolute bottom-2 left-2 w-[50%] h-[20%] flex justify-center items-center bg-white/50 backdrop-blur-sm rounded-lg">
+                  <div className="flex gap-4">
+                    <span className="text-white text-sm font-medium">15°C ⬇️</span>
+                    <span className="text-white text-sm font-medium">22°C ⬆️</span>
+                  </div>
+                </div>
+
+
+
+
+              </div>
+
+
+
+            </div>
+
+            {/* Sağ Sütun */}
+            <div className="flex flex-col gap-4 w-[80%] h-full">
+              {/* Üçüncü Kutu */}
+              <div className="w-full h-[20%] bg-gray-300 rounded-lg shadow-md"></div>
+              <div className="w-full h-[60%] bg-gray-300 rounded-lg shadow-md"></div>
+              {/* Dördüncü Kutu */}
+              <div className="w-full h-[20%] bg-gray-300 rounded-lg shadow-md"></div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
 
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        //login button color
+        weatherBlue:'#3EABE4'
+      },
+    },
   },
   plugins: [],
 }
